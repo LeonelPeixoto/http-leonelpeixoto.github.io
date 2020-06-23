610a01ff591f6afd3da1103e2380ed2da0272961
leonelpeixoto <ei12178@fe.up.pt>
2020-06-23 19:36:44 +0100

    .

---
+++ archviz/js/custom/panorama.js
@@ -0,0 +1,1 @@
+AFRAME.registerPrimitive('a-panorama',{defaultComponents:{panorama:{}}});AFRAME.registerComponent('panorama',{schema:{rotation:{type:'vec3'},position:{type:'vec3'},src:{type:'string'}}});
